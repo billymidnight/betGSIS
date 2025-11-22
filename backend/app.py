e203f157bb6b8b0fcc2358854d005c2090388db7
billymidnight <pritesh82tobayern@gmail.com>
2025-11-22 18:10:13 +0000

    APOAPOA

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,9 @@ def create_app():
                 "http://localhost:3000",
                 "http://127.0.0.1:3000",
                 "http://localhost:5173",
-                "http://127.0.0.1:5173"
+                "http://127.0.0.1:5173",
+                "https://betgsis2.vercel.app",
+                "https://betgsis2-qfq97111j-priteshs-projects-d318466e.vercel.app"
             ],
             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
             "allow_headers": ["Content-Type", "Authorization", "X-User-Email", "X-User-Name", "X-User-Role"],
